c54ea530953305646bb4dbb81a5e7c35ad30005e
Luísa Barros <qlullyq@gmail.com>
2019-05-01 16:02:53 -0300

    Encrypt password and add docker-compose

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: '127.0.0.1',
   username: 'docker',
   password: 'docker',
-  database: 'postgres',
+  database: 'gobarber',
   operatorAliases: false,
   define: {
     timestamps: true,
