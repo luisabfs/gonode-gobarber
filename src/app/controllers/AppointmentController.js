064da7d235602a9ce3add2062761b79be290fc2e
Luísa Barros <qlullyq@gmail.com>
2019-05-02 01:01:27 -0300

    Appointment form

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,12 @@
+/* eslint-disable class-methods-use-this */
+const { User } = require('../models');
+
+class AppointmentController {
+  async create(req, res) {
+    const provider = await User.findByPk(req.params.provider);
+
+    return res.render('appointments/create', { provider });
+  }
+}
+
+module.exports = new AppointmentController();
