0a3f89101c33cc8ddb1729812b5fd5df5430d932
Luísa Barros <qlullyq@gmail.com>
2019-05-04 05:07:52 -0300

    Add schedule feature

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ const guestMiddleware = require('./app/middlewares/guest');
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
 const DashboardController = require('./app/controllers/DashboardController');
+const ScheduleController = require('./app/controllers/ScheduleController');
 const FileController = require('./app/controllers/FileController');
 const AppointmentController = require('./app/controllers/AppointmentController');
 const AvailableController = require('./app/controllers/AvailableController');
@@ -35,6 +36,7 @@ routes.use('/app', authMiddleware);
 routes.get('/app/logout', SessionController.destroy);
 
 routes.get('/app/dashboard', DashboardController.index);
+routes.get('/app/schedule', ScheduleController.index);
 
 routes.get('/app/appointments/new/:provider', AppointmentController.create);
 routes.post('/app/appointments/new/:provider', AppointmentController.store);
