d045dddb2a096f7964a928a40764e30e92d3fb24
Luísa Barros <qlullyq@gmail.com>
2020-03-09 14:13:38 -0300

    feat: create user auth middleware

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
 import jwt from 'jsonwebtoken';
 
+import authConfig from '../../config/auth';
+
 import User from '../models/User';
 
 class SessionController {
@@ -21,8 +23,8 @@ class SessionController {
         name,
         email,
       },
-      token: jwt.sign({ id }, 'd01fde6a7a97af3292bbaccb2fc63f9b', {
-        expiresIn: '7d',
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
       }),
     });
   }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd01fde6a7a97af3292bbaccb2fc63f9b',
+  expiresIn: '7d',
+};
