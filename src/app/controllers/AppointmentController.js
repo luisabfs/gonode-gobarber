6d60df0d245ac61a52d702ba56dc5e42797ebf4f
Luísa Barros <qlullyq@gmail.com>
2020-03-13 13:47:45 -0300

    feat: create appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,6 @@
 import * as Yup from 'yup';
+
+import Appointment from '../models/Appointment';
 import User from '../models/User';
 
 class AppointmentController {
@@ -12,7 +14,7 @@ class AppointmentController {
       return res.status(400).json({ error: 'Validation error' });
     }
 
-    const { provider_id } = req.body;
+    const { provider_id, date } = req.body;
 
     const isProvider = await User.findOne({
       where: { id: provider_id, provider: true },
@@ -23,7 +25,13 @@ class AppointmentController {
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
 
-    return res.json();
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
   }
 }
 
