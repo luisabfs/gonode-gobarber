d7e7d736cadaa8c15874599e7e576f76503063cc
Luísa Barros <qlullyq@gmail.com>
2020-03-19 14:28:13 -0300

    chore: configure nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'a03a56f73f046b',
+    pass: '1cd170bd175884',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
