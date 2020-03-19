84247608e201e5ec86127094ee56147cfd72e171
Luísa Barros <qlullyq@gmail.com>
2020-03-19 13:53:51 -0300

    feat: cancel appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, isBefore, parseISO, format } from 'date-fns';
+import { startOfHour, isBefore, parseISO, format, subHours } from 'date-fns';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -92,6 +92,29 @@ class AppointmentController {
       content: `New appointment from ${user.name} on ${formattedDate}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment",
+      });
+    }
+
+    const subDate = subHours(appointment.date, 2);
+
+    if (isBefore(subDate, new Date())) {
+      return res
+        .status(401)
+        .json({ error: 'You can only cancel appointments 2 hours in advance' });
+    }
+
+    appointment.canceled_at = new Date();
+    await appointment.save();
 
     return res.json(appointment);
   }
